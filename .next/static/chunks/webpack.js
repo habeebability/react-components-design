8a24f691a4f25a5f89fa1bdbf6b457e5986bc508
Habeebability <awoyemi.habeeb@gmail.com>
2023-08-15 12:24:28 +0100

    add: the company and social data in the demographic component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7d43bc9e04f752df"; }
+/******/ 		__webpack_require__.h = function() { return "1614b7460a2969e3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
